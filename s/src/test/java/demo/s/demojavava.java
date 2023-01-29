bc829bfd821145065aefeef5b6d24062e53758e4
Maruti <mbkolavale@gmail.com>
2023-01-29 18:40:16 +0530

    update change in democ class

--- s/src/test/java/demo/s/demojavava.java
+++ s/src/test/java/demo/s/demojavava.java
@@ -5,7 +5,10 @@ public class demojavava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub{
 		System.out.println("Maruti kolavale");
-	
+		System.out.println("Maruti kolavale");
+		System.out.println("Maruti kolavale");
+		System.out.println("Maruti kolavale");
+		System.out.println("Maruti kolavale");
 }
 	}
 
