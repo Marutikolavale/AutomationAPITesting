3679272d3e6feaa07be4177d479212934861603c
Maruti <mbkolavale@gmail.com>
2023-01-29 18:47:46 +0530

    update 1

--- s/src/test/java/demo/s/demojavava.java
+++ s/src/test/java/demo/s/demojavava.java
@@ -5,25 +5,7 @@ public class demojavava {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub{
 		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");
-		System.out.println("Maruti kolavale");System.out.println("Maruti kolavale");
+
 		
 }
 	}
