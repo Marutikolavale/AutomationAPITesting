76850f83f85fcae6b54b9e3bf8bbe640a6f33f2e
Maruti <mbkolavale@gmail.com>
2023-01-29 18:29:27 +0530

    demotest ahe

---
+++ s/src/test/java/demo/s/demojavava.java
@@ -0,0 +1,12 @@
+package demo.s;
+
+public class demojavava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub{
+		System.out.println("Maruti kolavale");
+	
+}
+	}
+
+
